b5f6c37e7f24b0fe98b2b416e46deacbbd92c10d
Liz <105076696+ElizabethGarrison@users.noreply.github.com>
2023-04-24 00:41:22 -0400

    Update 2.0

--- NYC Water Fountains/main.js
+++ NYC Water Fountains/main.js
@@ -1,72 +1,62 @@
-$(document).ready(function(){
-    // Show loading screen
-    $("#loading").fadeIn();
+// Set the dimensions of the canvas
+var margin = {top: 20, right: 20, bottom: 20, left: 20},
+	width = window.innerWidth - margin.left - margin.right,
+	height = window.innerHeight - margin.top - margin.bottom;
 
-    // Load data set
-    d3.csv("data/Cool_It__NYC_2020_-_Cooling_Sites.csv", d3.autoType).then(function(data) {
-        // Hide loading screen
-        $("#loading").fadeOut();
+// Set the map projection
+var projection = d3.geoMercator()
+	.center([-73.94, 40.70])
+	.scale(60000)
+	.translate([width / 2, height / 2]);
 
-        // Initialize map
-        var width = window.innerWidth;
-        var height = window.innerHeight;
-        var svg = d3.select("#map").append("svg")
-            .attr("width", width)
-            .attr("height", height);
-        var projection = d3.geoMercator()
-            .scale(65000)
-            .center([-73.94, 40.7]);
-        var path = d3.geoPath().projection(projection);
+// Create a path generator
+var path = d3.geoPath()
+	.projection(projection);
 
-        // Load boroughs
-        d3.json("https://gist.githubusercontent.com/deldersveld/94bcbb7250bad11c4ddab154eea4f0ba/raw/171f0b7c8dc6e4e6d1efb2a55b76e8d6401ad6fc/nyc-boroughs.geojson").then(function(json) {
-            svg.selectAll("path")
-                .data(json.features)
-                .enter()
-                .append("path")
-                .attr("d", path)
-                .style("fill", "#ccc")
-                .style("stroke", "#000")
-                .style("stroke-width", 1);
+// Create the SVG element
+var svg = d3.select("#map").append("svg")
+	.attr("width", width)
+	.attr("height", height);
 
-            // Load data points
-            var g = svg.append("g");
-            var points = g.selectAll("circle")
-                .data(data)
-                .enter()
-                .append("circle")
-                .attr("cx", function(d) {
-                    return projection([d.X, d.Y])[0];
-                })
-                .attr("cy", function(d) {
-                    return projection([d.X, d.Y])[1];
-                })
-                .attr("r", 5)
-                .style("fill", "#f00")
-                .style("stroke", "#fff")
-                .style("stroke-width", 1)
-                .on("mouseover", function(d) {
-                    var tooltip = tippy(this, {
-                        content: "<b>" + d.PropertyName + "</b><br>" +
-                            "Feature Type: " + d.FeatureType + "<br>" +
-                            "Status: " + d.Status,
-                        allowHTML: true,
-                        placement: "top",
-                        maxWidth: "none",
-                        theme: "light-border"
-                    });
-                    tooltip.show();
-                })
-                .on("mouseout", function(d) {
-                    tippy(this).hide();
-                });
+// Create a tooltip
+var tooltip = d3.tip()
+	.attr("class", "d3-tip")
+	.offset([-10, 0])
+	.html(function(d) {
+		return "<strong>Property Name:</strong> " + d.PropertyName + "<br>" + "<strong>Feature Type:</strong> " + d.FeatureType + "<br>" + "<strong>Status:</strong> " + d.Status;
+	});
+svg.call(tooltip);
 
-            // Add zoom and pan
-            svg.call(d3.zoom()
-                .scaleExtent([1, 8])
-                .on("zoom", function() {
-                    g.attr("transform", d3.event.transform);
-                }));
-        });
-    });
+// Load the data
+d3.csv("Cool_It__NYC_2020_-_Cooling_Sites.csv", function(data) {
+	// Draw the map
+	d3.json("nyc_boroughs.geojson", function(error, json) {
+		if (error) throw error;
+		svg.append("g")
+			.attr("class", "boroughs")
+			.selectAll("path")
+			.data(json.features)
+			.enter().append("path")
+			.attr("d", path);
+
+		// Draw the data points
+		svg.append("g")
+			.attr("class", "points")
+			.selectAll("circle")
+			.data(data)
+			.enter().append("circle")
+			.attr("class", "circle")
+			.attr("r", 5)
+			.attr("cx", function(d) { return projection([d.X, d.Y])[0]; })
+			.attr("cy", function(d) { return projection([d.X, d.Y])[1]; })
+			.on("mouseover", function(d) {
+				tooltip.show(d);
+			})
+			.on("mouseout", function(d) {
+				tooltip.hide(d);
+			});
+	});
+
+	// Remove the loading screen
+	$("#loader-wrapper").fadeOut();
 });
