338c81b387743a424ac4cc8c3cdd6af3db1b72fa
Liz <105076696+ElizabethGarrison@users.noreply.github.com>
2023-05-09 18:34:04 -0400

    Final Project Ver.1

--- NYC_Water_Fountains/main.js
+++ NYC_Water_Fountains/main.js
@@ -2,6 +2,22 @@
 const width = window.innerWidth * 0.9,
   height = window.innerHeight * 0.7,
   margin = { top: 20, bottom: 50, left: 60, right: 40 };
+  const key = {
+    'Rectangular Fountain with Child Step': '#fdae61',
+    'Circular Fountain': '#abd9e9',
+    'Circular Fountain with Child Step and Overhang': '#2c7bb6',
+    'Rectangular Fountain with Overhang': '#d7191c',
+    'Wheelchair Accessible Fountain': '#ffffbf',
+    'Water Bottle Stations, Etc.': '#bdbdbd',
+  };
+  const colorLegend = [
+    { type: 'A', color: '#fdae61' }, // Rectangular Fountain with Child Step
+    { type: 'B', color: '#abd9e9' }, // Circular Fountain
+    { type: 'C', color: '#2c7bb6' }, // Circular Fountain with Child Step and Overhang
+    { type: 'D', color: '#d7191c' }, // Rectangular Fountain with Overhang
+    { type: 'E', color: '#ffffbf' }, // Wheelchair Accessible Fountain
+    { type: 'Other', color: '#bdbdbd' }, // Water Bottle Stations, Etc.
+  ];
 
 // SHOW LOADING SCREEN FOR AT MOST 2.5 SECONDS
 const loadingScreen = d3.select("#loading-screen")
@@ -46,28 +62,52 @@ setTimeout(() => {
       .attr("fill", "#EEE5E9")
       .attr("stroke", "#08415C");
 
-    // APPEND DATA AS SHAPE
-    const drinkingFountainsShapes = svg.selectAll("circle.drinkingFountainsShapes")
-      .data(drinkingFountains)
-      .join("circle")
-      .attr("class", "drinkingFountainsShapes")
-      .attr("r", 2)
-      .attr("transform", (d) => {
-        const [x, y] = projection([d.Long, d.Lat]);
-        return `translate(${x}, ${y})`;
-      })
-      .attr("fill", "#6baed6")
-      .on("mouseover", function (event, d) {
-        const tooltip = d3.select("#tooltip");
-        tooltip.style("display", "block");
-        tooltip.html(`<p><strong>Description:</strong> ${d.DESCRIPTIO}</p><p><strong>Sign Name:</strong> ${d.SIGNNAME}</p>`);
-        const parentPos = this.parentNode.getBoundingClientRect();
-        tooltip.style("left", event.offsetX + parentPos.left - 150 + "px");
-        tooltip.style("top", event.offsetY + parentPos.top - 150 + "px"); 
-      })
-                  .on("mouseout", function () {
-        const tooltip = d3.select("#tooltip");
-        tooltip.style("display", "none");
-      });
+// APPEND DATA AS SHAPE
+const drinkingFountainsShapes = svg.selectAll("circle.drinkingFountainsShapes")
+  .data(drinkingFountains)
+  .join("circle")
+  .attr("class", d => "drinkingFountainsShapes type-" + (d.TYPE ? d.TYPE.replace(/\s+/g, '-') : 'Other'))
+  .attr("r", 4)
+  .attr("transform", (d) => {
+    const [x, y] = projection([d.Long, d.Lat]);
+    return `translate(${x}, ${y})`;
+  })
+  .attr("fill", d => key[d.TYPE] || key.Other)
+  .on("mouseover", function (event, d) {
+    const tooltip = d3.select("#tooltip");
+    tooltip.style("display", "block");
+    tooltip.html(`<p><strong>Description:</strong> ${d.DESCRIPTION}</p><p><strong>Sign Name:</strong> ${d.SIGNNAME}</p>`);
+    const parentPos = this.parentNode.getBoundingClientRect();
+    tooltip.style("left", event.offsetX + parentPos.left - 150 + "px");
+    tooltip.style("top", event.offsetY + parentPos.top - 150 + "px"); 
+  })
+  .on("mouseout", function () {
+    const tooltip = d3.select("#tooltip");
+    tooltip.style("display", "none");
+  });
+
+// CREATE LEGEND
+const legendMargin = 20;
+const legend = svg.append("g")
+  .attr("class", "legend")
+  .attr("transform", `translate(${width - margin.right - 300 - legendMargin}, ${margin.top + legendMargin})`);
+
+const legendKeys = Object.keys(key);
+
+legendKeys.forEach((k, i) => {
+  const legendRow = legend.append("g")
+    .attr("transform", `translate(0, ${i * 20})`);
+
+  legendRow.append("rect")
+    .attr("width", 10)
+    .attr("height", 10)
+    .attr("fill", key[k]);
+
+    legendRow.append("text")
+    .attr("x", 16)
+    .attr("y", 10)
+    .text(k);  
+});
+
   });
 }, 2500); // 2.5 SECOND DELAY BEFORE LOADING THE MAP
